ea0e57ef51f1943641a7ac1a40b80f357b934256
Nikitos09 <nikita0999.99@mail.ru>
2018-05-20 13:49:54 +0300

    Homework 5

---
+++ Lesson 5/Task 3/main.js
@@ -0,0 +1,23 @@
+var text = prompt('Введите текст',);
+
+function vowelCount(text) {
+    var textArr = text.toLowerCase().split(''),
+        lowerArr = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е'],
+        textLength = textArr.length,
+        lowerLength = lowerArr.length,
+        i, j, counter = 0;
+
+    for (i = 0; i < textLength; i++) {
+        for (j = 0; j < lowerLength; j++) {
+            if (textArr[i] == lowerArr[j]) {
+                counter++
+            }
+        }
+
+    }
+    return ' Количество гласных в слове "' + text + '" равно ' + counter;
+}
+
+alert(vowelCount(text));
+
+
