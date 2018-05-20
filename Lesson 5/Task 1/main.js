f7beb03159f56e72abb0832186c00ca17d690f50
Nikitos09 <nikita0999.99@mail.ru>
2018-05-20 13:49:15 +0300

    Dell Homework 5

--- Lesson 5/Task 1/main.js
+++
@@ -1,9 +0,0 @@
-var wordBase = prompt('Введите ваше слово',).toLowerCase();
-
-function isReverse(wordBase) {
-    var wordReverse = wordBase.split('').reverse().join('');
-    wordBase == wordReverse ?
-        alert(' Слово ' + wordBase + ' - полиндром!') :
-        alert(' Слово ' + wordBase + ' -  НЕ полиндром!');
-}
-    isReverse(wordBase);
